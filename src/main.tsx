2ff7f7792d634ec3c8f57c7260c64a320fbbc0d5
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-02-10 07:23:35 +0200

    feat: add NotFound page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import NotFound from '@modules/core/components/NotFound/NotFound.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,6 +14,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>

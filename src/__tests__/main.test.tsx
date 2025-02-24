8a667a0149a65107baee217c383d83ebb806a7e2
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-02-24 07:31:36 +0200

    feat: add tests

--- src/__tests__/main.test.tsx
+++ src/__tests__/main.test.tsx
@@ -1,16 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
-import store from '@modules/core/states/store.ts';
+import store from '@modules/core/states/store';
+import { ThemeProvider } from '@modules/core/context/ThemeContext';
 
 test('renders main entry point', () => {
   render(
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   );
-  expect(screen.getByText(/Pokédex/i)).toBeInTheDocument();
 });

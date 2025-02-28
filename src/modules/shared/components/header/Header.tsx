9d065078122cac97be72063aa5a61725309d5d28
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-02-28 22:58:46 +0200

    feat: add Link in header

--- src/modules/shared/components/header/Header.tsx
+++ src/modules/shared/components/header/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from '../../../../assets/Pokeball.svg';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <img src={logo} alt="Logo" />
-      <h1>Pokédex</h1>
+      <Link to="/">
+        <img src={logo} alt="Logo" />
+      </Link>
+      <Link to="/">
+        <h1>Pokédex</h1>
+      </Link>
     </header>
   );
 };

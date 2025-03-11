bb6ab980e78666efad53e9e7d2e7a5c54f45ac15
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-03-11 21:49:25 +0200

    fix: babel

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 export default {
   presets: [
+    ['next/babel'],
     ['@babel/preset-env', { targets: { node: 'current' } }],
     ['@babel/preset-react', { runtime: 'automatic' }],
     '@babel/preset-typescript',
-    ['next/babel'],
   ],
 };
